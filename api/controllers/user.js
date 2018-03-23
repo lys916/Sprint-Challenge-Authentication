187c2adb9667b480c267a2591531d7d12efdc283
Lo Saephan <teamsky916@gmail.com>
2018-03-23 08:56:21 -0700

    added user schemas. added bcrypt hash passsword. added bcrypt check password. added create user api functionaltiy. Able to to log in and get a token back. Main sprint done.. on to stretch.

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,6 +5,14 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  const newUser = new User({username, password});
+  newUser.save().then(savedUser => {
+  	if (!savedUser) res.json({ message: "Unable to create user" });
+  	res.json(savedUser);
+  }).catch(err => {
+  	console.log(err);
+  	res.json({ message: "Unable to create user", error: err });
+  });
 };
 
 module.exports = {
